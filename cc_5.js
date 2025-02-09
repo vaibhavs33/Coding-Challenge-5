46b22cf82e1e44925222dc5204cdce2a9bf643ae
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-02-08 22:39:04 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,16 @@
+//Task 1 - Customer Profile
+const customer = {
+    name: "John Doe", //Customer's name
+    age: 35, //Customer's age
+    email: "john.doe@email.com" //Customer's email address
+};
+
+//Logging the customer's name to the console
+console.log(`Customer's name: ${customer.name}`);
+
+//Logging the customer's age to the console
+console.log(`Customer's age: ${customer.age}`);
+
+//Logging the customer's email to the console
+console.log(`Customer's email: ${customer.email}`);
+
